0da2c451169fb7545d1d261a577bd29442f9025a
Valeriu Timbuc <valeriu@vtimbuc.com>
2014-11-26 14:32:19 +0000

    move items.js to root

--- items.js
+++ items.js
@@ -6,6 +6,15 @@ var $items, $defaultItem = 'careers_html';
 // ---------------------------------------------------------
 $items = {
 
+  bingo_wordpress: {
+    name      : 'Bingo',
+    tag       : 'WordPress',
+    img       : 'https://0.s3.envato.com/files/109941662/screenshots/00_preview.__large_preview.jpg',
+    url       : 'http://188.226.173.21/bingo/',
+    purchase  : 'http://themeforest.net/item/bingo-auction-wordpress-theme/9263645?ref=uouapps',
+    tooltip   : 'Build Your Own Auction Site With Wordpress',
+  },
+
   careers_html: {
     name      : 'Careers',
     tag       : 'HTML',
@@ -21,8 +30,7 @@ $items = {
     img       : 'https://0.s3.envato.com/files/87934617/01_preview.__large_preview.jpg',
     url       : 'http://uouapps.com/casa2/',
     purchase  : 'http://themeforest.net/item/casa-swap-book-rent-html-template-/6703459?ref=uouapps',
-    tooltip   : 'Swap, Book & Rent',
-    responsive: false
+    tooltip   : 'Swap, Book & Rent'
   }
 
 };
